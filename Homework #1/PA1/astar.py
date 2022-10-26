c42927919af8c502c2e256c0ea5c7c4772bd832b
Achyuth Kolluru <kolluruachyuth@gmail.com>
2022-10-25 18:50:21 -0700

    Finished heuristic and A star search

--- Homework #1/PA1/astar.py
+++ Homework #1/PA1/astar.py
@@ -24,5 +24,31 @@ def a_star_search(problem: RouteProblem, h, repeat_check=False):
     the provided boolean argument is true."""
 
     # PLACE YOUR CODE HERE
+    solNode = Node(problem.start, h_eval= h.h_cost(problem.start))
+    arr = Frontier(solNode, sort_by='f')
+    visited = set()
+    visited.add(solNode)
+
+    if problem.is_goal(solNode.loc):
+        return solNode
+    while not arr.is_empty():
+        solNode = arr.pop()
+        if problem.is_goal(solNode.loc):
+            return solNode
+        
+        for x in solNode.expand(problem, h):
+            if repeat_check:
+                if x in visited:
+                    if arr.contains(x) and x.value(sort_by='f') < arr.__getitem__(x):
+                        arr.__delitem__(x)
+                        arr.add(x)
+                else:
+                    # x.h_eval = h.h_cost(solNode.loc)
+                    arr.add(x)
+                    visited.add(x)
+            else:
+                # x.h_eval = h.h_cost(solNode.loc)
+                arr.add(x)
+                visited.add(x)
 
     return None
--- Homework #1/PA1/heuristic.py
+++ Homework #1/PA1/heuristic.py
@@ -18,7 +18,7 @@
 
 import route
 
-
+'''
 class HeuristicFunction:
     """A heuristic function object contains the information needed to
     evaluate a state (location) in terms of its proximity to an optimal
@@ -55,5 +55,59 @@ class HeuristicFunction:
             #figures out the euclidian value from 2 locations goalLoc and the value that is being passed in 
             # print(self.problem.map.euclidean_distance(loc, goalLoc))
             value = min(speed)
-            return value
+            return value'''
 
+class HeuristicFunction:
+    """A heuristic function object contains the information needed 
+    evaluate a state (location) in terms of its proximity to an optimal
+    goal state."""
+
+    def __init__(self, problem=None):
+        self.problem = problem
+        self.goal = problem.goal
+        self.time = 0
+        #we need to calculate the time_cost for the heuristic function
+        #can't only use distance for heuristic
+        all_streets = problem.map.locations()
+        #calculate all possible locations
+        for roads in all_streets:
+            all_locations = (problem.map.get_result(roads))
+            for path in all_locations.values():
+                #calculate the distance between the two locations
+                distance = problem.map.euclidean_distance(roads, path)
+                # get the time by dividing it by speed which is the road cost from start to end
+                time = distance / problem.map.get(roads, path)
+                # return the biggest time value that way the heirstic function can never be underestimated
+                if time > self.time:
+                    self.time = time
+
+
+    def h_cost(self, loc=None):
+        """An admissible heuristic function, estimating the cost from
+        the specified location to the goal state of the problem."""
+        # a heuristic value of zero is admissible but not informative
+        value = 0.0
+        if loc is None:
+            return value
+        else:
+            # PLACE YOUR CODE FOR CALCULATING value OF loc HERE
+            # An admissable heurisitic function can be determined through the euclidian distance to get an estimate.
+            # gets the goal location of the problem
+            # # goalLoc = self.problem.goal
+            
+            # all_streets = self.problem.actions(loc)
+            # # print(all_streets)
+            # speed = []
+            # for roads in all_streets:
+                
+            #     places = self.problem.result(loc, roads)
+            #     time_cost = self.problem.action_cost(loc, places)
+            #     distance = self.problem.map.euclidean_distance(loc, places)
+            #     speed.append(distance/time_cost)
+            
+            
+            # figures out the euclidian value from 2 locations goalLoc and the value that is being passed in 
+            # print(self.problem.map.euclidean_distance(loc, goalLoc))
+            distance = self.problem.map.euclidean_distance(loc, self.goal)
+            value = distance / self.time
+            return value
